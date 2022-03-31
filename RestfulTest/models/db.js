d96d90095962891d9d91b71a28176b74e01eaa44
Bach <bach19112002@gmail.com>
2022-04-01 02:16:47 +0700

    hhihi

--- RestfulTest/models/db.js
+++
@@ -1,7 +0,0 @@
-var mysql = require('mysql');
-var db = mysql.createConnection({
-   host: 'localhost', user: 'root', password: '', 
-   database: 'bookshop'
-}); 
-db.connect(() => console.log('Da ket noi database !'));
-module.exports = db; 
